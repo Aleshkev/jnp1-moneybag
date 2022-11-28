d2fec378390a14d90107241ab2d53083e824ed0e
Aleshkev <43296251+Aleshkev@users.noreply.github.com>
2022-11-28 20:06:09 +0100

    forgot to commit moneybag.cc ;c

--- moneybag.cc
+++ moneybag.cc
@@ -1,33 +1,14 @@
 #include "moneybag.h"
 
-Moneybag &Moneybag::operator+=(const Moneybag &that) {
-  set_value(add_unsigned(livres, that.livres),
-            add_unsigned(soliduses, that.soliduses),
-            add_unsigned(deniers, that.deniers));
-  return *this;
-}
-
-Moneybag &Moneybag::operator-=(const Moneybag &that) {
-  set_value(subtract_unsigned(livres, that.livres),
-            subtract_unsigned(soliduses, that.soliduses),
-            subtract_unsigned(deniers, that.deniers));
-  return *this;
-}
-
-Moneybag &Moneybag::operator*=(Moneybag::coin_number_t that) {
-  set_value(multiply_unsigned(livres, that),
-            multiply_unsigned(soliduses, that),
-            multiply_unsigned(deniers, that));
-  return *this;
-}
-
+namespace {
 std::string quantity(const std::string &name, const std::string &plural_suffix,
                      Moneybag::coin_number_t x) {
   return std::to_string(x) + " " + name + (x == 1 ? "" : plural_suffix);
 }
+} // namespace
 
 std::ostream &operator<<(std::ostream &o, const Moneybag &bag) {
-  return o << "(" << quantity("livre", "s", bag.livre_number()) << ", "
+  return o << "(" << quantity("livr", "es", bag.livre_number()) << ", "
            << quantity("solidus", "es", bag.solidus_number()) << ", "
            << quantity("denier", "s", bag.denier_number()) << ")";
 }
